66b4b897c05e53a42728ac74d910f6ae6d783a25
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-10 03:21:54 -0700

    Rename class name

---
+++ src/test/java/features/CucumberSerenityBDDTestsRunner.java
@@ -0,0 +1,10 @@
+package features;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features", plugin = {"pretty"})
+public class CucumberSerenityBDDTestsRunner {
+}
