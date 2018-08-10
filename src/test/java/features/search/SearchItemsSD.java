51371b7371b82de973793e977c688eb8a8717715
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-10 03:24:34 -0700

    Created SearchItemSD class
    This will show pending status in living documentation as of now

---
+++ src/test/java/features/search/SearchItemsSD.java
@@ -0,0 +1,27 @@
+package features.search;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SearchItemsSD {
+
+    @Given("^Carla wants to buy T-shirt$")
+    public void carla_wants_to_buy_T_shirt() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^she searches for T-shirts using the navigation menu$")
+    public void she_searches_for_T_shirts_using_the_navigation_menu() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^she should see the list of T-shirts with prices available for sale$")
+    public void she_should_see_the_list_of_T_shirts_with_prices_available_for_sale() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
