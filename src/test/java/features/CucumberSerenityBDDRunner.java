6c3d4763ff4c5ee43e3152b9c61c9a4de2773333
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-13 23:14:33 -0700

    Added tags to run only the completed tests

--- src/test/java/features/CucumberSerenityBDDRunner.java
+++ src/test/java/features/CucumberSerenityBDDRunner.java
@@ -5,6 +5,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features", plugin = {"pretty"})
+@CucumberOptions(features="src/test/resources/features", plugin = {"pretty"}, tags ={"@smoke"})
 public class CucumberSerenityBDDRunner {
 }
