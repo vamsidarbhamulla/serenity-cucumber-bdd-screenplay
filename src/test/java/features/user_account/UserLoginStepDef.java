463ba03621d44b5771f97a6cafc85ac34e2c6a3c
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-17 02:46:06 -0700

    Updated UserLoginStepDef to use single methods for both the scenarios

--- src/test/java/features/user_account/UserLoginStepDef.java
+++ src/test/java/features/user_account/UserLoginStepDef.java
@@ -26,45 +26,24 @@ public class UserLoginStepDef {
     }
 
 
-    @Given("^that (.*) is a registered member$")
+    @Given("^that (.*) is a registered (?:member|admin)$")
     public void user_is_a_registered_member(String actor){
-        prepare_actor_to_sign_in(actor);
-    }
-
-    @Given("^that (.*) is a registered admin")
-    public void user_is_a_registered_admin(String actor){
-        prepare_actor_to_sign_in(actor);
-    }
-
-
-    private void prepare_actor_to_sign_in(String actor){
         theActorCalled(actor)
                 .wasAbleTo(Start.prepareToSignIn());
     }
 
-    @When("^s?he logs in with valid credentials$")
+    @When("^(?:he|she|they) logs in with valid credentials$")
     public void user_has_signed_in_with_their_account(DataTable credentials){
         theActorInTheSpotlight()
                 .whoCan(Authenticate.with(credentials))
                 .attemptsTo(LogIn.withCredentials());
     }
 
-    @Then("^s?he should be able to view her account profile$")
+    @Then("^(?:he|she|they) should be able to view (?:his|her|their) account profile$")
     public void user_should_be_able_to_view_their_account_profile(){
-        actor_should_see_their_profile();
-    }
-
-    @Then("^?he should be able to view his account profile$")
-    public void admin_should_be_able_to_view_their_account_profile(){
-        actor_should_see_their_profile();  // Admin account should see a different view than customer but for our test website they both have same view.
-    }
-
-
-    private void actor_should_see_their_profile(){
         theActorInTheSpotlight()
                 .should(seeThat(UserAccount.loaded(),
                         displays("title", equalTo("My account - My Store"))));
     }
-
 
 }
