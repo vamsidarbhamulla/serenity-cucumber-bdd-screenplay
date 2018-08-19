ca9887623b2532b3ec7164d409df6ea896343155
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-18 23:23:00 -0700

    Updated Navigate Menu Task to handle menu option is visible

--- src/main/java/screenplay/tasks/NavigateMenu.java
+++ src/main/java/screenplay/tasks/NavigateMenu.java
@@ -1,16 +1,30 @@
 package screenplay.tasks;
 
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Hover;
+import net.serenitybdd.screenplay.conditions.Check;
 import screenplay.user_interfaces.GlobalMenu;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+import static net.serenitybdd.screenplay.questions.WebElementQuestion.valueOf;
 
 public class NavigateMenu {
 
      public static Task toBlousesItem(){
         return Task.where("Navigate Menu to blouses option",
-                Hover.over(GlobalMenu.WOMEN)
-                ,Hover.over(GlobalMenu.BLOUSES),
-                Click.on(GlobalMenu.BLOUSES));
+                Hover.over(GlobalMenu.WOMEN),
+                Check.whether(
+                        valueOf(GlobalMenu.WOMEN),isVisible())
+                        .andIfSo( Hover.over(GlobalMenu.WOMEN)),
+                Check.whether(
+                        valueOf(GlobalMenu.BLOUSES),isVisible())
+                        .andIfSo( Hover.over(GlobalMenu.BLOUSES))
+                        .andIfSo(Click.on(GlobalMenu.BLOUSES))
+
+        );
+
     }
+
 }
