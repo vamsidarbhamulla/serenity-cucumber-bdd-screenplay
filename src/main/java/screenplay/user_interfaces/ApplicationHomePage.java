925c2721f6fce0ca62cea41cc9aa2e62f811f3af
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-24 02:40:53 -0700

    Updated default url port no.

--- src/main/java/screenplay/user_interfaces/ApplicationHomePage.java
+++ src/main/java/screenplay/user_interfaces/ApplicationHomePage.java
@@ -3,5 +3,5 @@ package screenplay.user_interfaces;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("http://localhost:900")
+@DefaultUrl("http://localhost:90")
 public class ApplicationHomePage extends PageObject {}
