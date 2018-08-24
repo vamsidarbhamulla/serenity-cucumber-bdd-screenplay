81b192dff2b666c0e4300b74bf2fa1cabc5ebbcb
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-23 23:04:20 -0700

    Corrected tags in CucumberSerenityBDDSimpleRunner

--- src/test/java/CucumberSerenityBDDSimpleRunner.java
+++ src/test/java/CucumberSerenityBDDSimpleRunner.java
@@ -3,6 +3,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features", plugin = {"pretty"}, tags ={"@smoke-s"})
+@CucumberOptions(features="src/test/resources/features", plugin = {"pretty"}, tags ={"@smoke"})
 public class CucumberSerenityBDDSimpleRunner {
 }
