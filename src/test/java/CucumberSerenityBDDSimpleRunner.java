d960d31ce2ec73f246b7c5decd9ec9b23629e6be
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-23 18:55:51 -0700

    Renamed CucumberSerenityBDDRunner to BDDSimpleRunner

--- src/test/java/CucumberSerenityBDDSimpleRunner.java
+++ src/test/java/CucumberSerenityBDDSimpleRunner.java
@@ -1,10 +1,8 @@
-package features;
-
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features", plugin = {"pretty"}, tags ={"@smoke"})
-public class CucumberSerenityBDDRunner {
+@CucumberOptions(features="src/test/resources/features", plugin = {"pretty"}, tags ={"@smoke-s"})
+public class CucumberSerenityBDDSimpleRunner {
 }
