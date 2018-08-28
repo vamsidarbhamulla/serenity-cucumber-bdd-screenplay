b6c4c2f138bb69159d56da0182857b42da4c9c63
Vamsi Krishna Darbhamulla <vamsi.krishna0207@gmail.com>
2018-08-28 00:42:43 -0700

    Fixing the default url port number

--- src/main/java/screenplay/user_interfaces/GlobalHeaderNavigationBar.java
+++ src/main/java/screenplay/user_interfaces/GlobalHeaderNavigationBar.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.annotations.findby.By;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
 
-@DefaultUrl("http://localhost:900")
+@DefaultUrl("http://localhost:90")
 public class GlobalHeaderNavigationBar extends PageObject {
 
     public static By GET_LOGIN_SCREEN = By.jquery("#_desktop_user_info > div > a > span");
